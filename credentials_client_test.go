3c1a85f8288abbc7580075b550432892b8e4d4af
Andrew Waters <waters@me.com>
2019-03-29 18:05:24 +0000

    Split out the credential tests

--- credentials_client_test.go
+++ credentials_client_test.go
@@ -14,13 +14,3 @@ func TestNewClientCredentials(t *testing.T) {
 		t.Error("Incorrect grant type")
 	}
 }
-
-func TestNewImplicitCredentials(t *testing.T) {
-	creds := NewImplicitCredentials("abc")
-	if creds.clientID != "abc" {
-		t.Error("Incorrect client id")
-	}
-	if creds.grantType() != implicitGrantType {
-		t.Error("Incorrect grant type")
-	}
-}
