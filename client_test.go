fb8b59037a9fd0d45748609aa3c834250e0c8c45
Andrew Waters <waters@me.com>
2019-03-29 12:36:58 +0000

    Adds tests for debugging

--- client_test.go
+++ client_test.go
@@ -18,13 +18,38 @@ func testClient() Client {
 
 func TestClientDefaults(t *testing.T) {
 	c := testClient()
+
+	// test the default endpoint
 	expectedEndpoint := "https://api.moltin.com"
 	if c.Endpoint != expectedEndpoint {
 		t.Errorf("Incorrect default endpoint: %s (expected %s)", c.Endpoint, expectedEndpoint)
 	}
+
+	// test the API version
 	expectedAPIVersion := "v2"
 	if c.APIVersion != expectedAPIVersion {
 		t.Errorf("Incorrect API version: %s (expected %s)", c.APIVersion, expectedAPIVersion)
+	}
+
+	// test the defaul debug status
+	if c.Debug != false {
+		t.Errorf("Incorrect debug val: %t (expected %t)", c.Debug, false)
+	}
+}
+
+func TestClientDebug(t *testing.T) {
+	c := testClient()
+
+	// test enabling debug
+	c.EnableDebug()
+	if c.Debug != true {
+		t.Errorf("Incorrect debug val: %t (expected %t)", c.Debug, true)
+	}
+
+	// test disabling debug
+	c.DisableDebug()
+	if c.Debug != false {
+		t.Errorf("Incorrect debug val: %t (expected %t)", c.Debug, false)
 	}
 }
 
