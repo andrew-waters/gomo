f7faa22b7fc1c82acee95a6477d19789bc8650c1
Andrew Waters <waters@me.com>
2019-04-05 20:04:38 +0100

    Add supported languages to settings

--- entities/settings.go
+++ entities/settings.go
@@ -1,6 +1,6 @@
 package entities
 
-// Settings is a Moltin Setting Configuration - https://docs.moltin.com/catalog/brands
+// Settings is a Moltin Setting Configuration - https://docs.moltin.com/advanced/settings
 type Settings struct {
 	Type                string   `json:"type"`
 	PageLength          int      `json:"page_length"`
@@ -11,4 +11,59 @@ type Settings struct {
 // SetType sets the resource type on the struct
 func (s *Settings) SetType() {
 	s.Type = settingsType
+}
+
+// SupportedLanguages returns the languages that are supported on the additional_languages field
+func (s *Settings) SupportedLanguages() []string {
+	return []string{
+		"ab",
+		"af",
+		"sq",
+		"hy",
+		"av",
+		"ae",
+		"ay",
+		"az",
+		"eu",
+		"be",
+		"bs",
+		"bg",
+		"ca",
+		"co",
+		"cs",
+		"da",
+		"nl",
+		"en",
+		"et",
+		"fj",
+		"fi",
+		"fr",
+		"de",
+		"gd",
+		"ga",
+		"el",
+		"ht",
+		"hr",
+		"hu",
+		"is",
+		"it",
+		"lv",
+		"lt",
+		"lb",
+		"no",
+		"fa",
+		"pl",
+		"pt",
+		"ro",
+		"ru",
+		"sk",
+		"sl",
+		"sm",
+		"es",
+		"sc",
+		"sr",
+		"sv",
+		"uk",
+		"cy",
+	}
 }
