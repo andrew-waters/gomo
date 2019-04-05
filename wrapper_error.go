6ced4d675b612573323af9ffc91498ba2b0788ce
Andrew Waters <waters@me.com>
2019-04-05 20:03:53 +0100

    APIError now implements the string interface

--- wrapper_error.go
+++ wrapper_error.go
@@ -1,5 +1,7 @@
 package gomo
 
+import "fmt"
+
 // APIError is an error returned by the API so that you can include error speciifc logic in your own implementation
 //
 // 	if error.Status == 404 {
@@ -9,4 +11,8 @@ type APIError struct {
 	Status int    `json:"status"`
 	Detail string `json:"detail"`
 	Title  string `json:"title"`
+}
+
+func (e APIError) String() string {
+	return fmt.Sprintf("{ Status: `%d`, Title: `%s`, Detail: `%s` }", e.Status, e.Title, e.Detail)
 }
