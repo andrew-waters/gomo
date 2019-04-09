429b857fb26a8faae75963be338a369f333f800a
Andrew Waters <waters@me.com>
2019-04-09 12:13:20 +0100

    Adds tests for string handling

---
+++ wrapper_error_test.go
@@ -0,0 +1,31 @@
+package gomo
+
+import "testing"
+
+var successfulUpdateTransactionTests = []struct {
+	status   int
+	title    string
+	detail   string
+	expected string
+}{
+	{200, "problem", "info", "{ Status: `200`, Title: `problem`, Detail: `info` }"},
+	{200, "problem", "", "{ Status: `200`, Title: `problem`, Detail: `` }"},
+	{200, "", "", "{ Status: `200`, Title: ``, Detail: `` }"},
+	{300, "", "", "{ Status: `300`, Title: ``, Detail: `` }"},
+}
+
+func TestAPIErrorString(t *testing.T) {
+
+	for _, test := range successfulUpdateTransactionTests {
+
+		e := APIError{
+			Status: test.status,
+			Title:  test.title,
+			Detail: test.detail,
+		}
+
+		if e.String() != test.expected {
+			t.Errorf("APIError to string returned `%s` (expected `%s`", e.String(), test.expected)
+		}
+	}
+}
