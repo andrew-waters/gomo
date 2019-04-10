ea55aa42ded5c9254f2b1eb8c5f6a949f8918369
Andrew Waters <andrew-waters@users.noreply.github.com>
2019-04-10 12:03:10 +0100

    Adds tests for entity types and supported languages (#18)

---
+++ entities/settings_test.go
@@ -0,0 +1,14 @@
+package entities
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSupportedLanguages(t *testing.T) {
+	s := Settings{}
+	expected := []string{"ab", "af", "sq", "hy", "av", "ae", "ay", "az", "eu", "be", "bs", "bg", "ca", "co", "cs", "da", "nl", "en", "et", "fj", "fi", "fr", "de", "gd", "ga", "el", "ht", "hr", "hu", "is", "it", "lv", "lt", "lb", "no", "fa", "pl", "pt", "ro", "ru", "sk", "sl", "sm", "es", "sc", "sr", "sv", "uk", "cy"}
+	if !reflect.DeepEqual(s.SupportedLanguages(), expected) {
+		t.Errorf("Settings did not return the correct supported languages. Got: `%s`", s.SupportedLanguages())
+	}
+}
