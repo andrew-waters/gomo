2bd165b054c03895ae860105d76377802d4139b9
James Carter <james.carter@moltin.com>
2019-06-24 12:43:34 +0100

    Add functional options

--- credentials_client_test.go
+++ credentials_client_test.go
@@ -2,14 +2,8 @@ package gomo
 
 import "testing"
 
-func TestNewClientCredentials(t *testing.T) {
-	creds := NewClientCredentials("abc", "def")
-	if creds.clientID != "abc" {
-		t.Error("Incorrect client id")
-	}
-	if creds.clientSecret != "def" {
-		t.Error("Incorrect client secret")
-	}
+func TestClientCredentials(t *testing.T) {
+	creds := clientCredentials{}
 	if creds.grantType() != clientCredentialsGrantType {
 		t.Error("Incorrect grant type")
 	}
--- credentials_implicit.go
+++ credentials_implicit.go
@@ -2,14 +2,6 @@ package gomo
 
 import "net/url"
 
-// NewImplicitCredentials creates and returns an implicitCredentials struct from the passed in client ID
-func NewImplicitCredentials(clientID string) implicitCredentials {
-	return implicitCredentials{
-		clientID: clientID,
-	}
-}
-
-// ImplicitCredentials gives you read access (and some write access) and is for untrusted environments
 type implicitCredentials struct {
 	clientID string
 }
