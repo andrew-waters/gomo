21d5039a17fdbf850ccb706ed6b7bf555c7383b5
Matthew Foyle <matt@moltin.com>
2019-06-25 19:01:39 +0300

    Add modifier entity

---
+++ entities/modifier.go
@@ -0,0 +1,14 @@
+package entities
+
+// Modifier represents a modifier for a variation option: https://docs.moltin.com/api/catalog/product-variations/modifiers
+type Modifier struct {
+	ID           string      `json:"id"`
+	Type         string      `json:"type"`
+	ModifierType string      `json:"modifier_type"`
+	Value        interface{} `json:"value"`
+}
+
+// SetType sets the resource type on the struct
+func (m *Modifier) SetType() {
+	m.Type = "modifier"
+}
