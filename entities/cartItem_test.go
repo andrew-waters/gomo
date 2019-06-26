50e73a4d8ba30f3c8b7bc59a4aa04e6ab49a2dbe
Matthew Foyle <matt@moltin.com>
2019-06-26 12:07:06 +0300

    Cleanup

--- entities/cartItem_test.go
+++ entities/cartItem_test.go
@@ -6,6 +6,6 @@ func TestCartItemType(t *testing.T) {
 	ci := CartItem{}
 	ci.SetType()
 	if ci.Type != "cart_item" {
-		t.Errorf("Cart did not return correct type: `%s`", ci.Type)
+		t.Errorf("CartItem did not return correct type: `%s`", ci.Type)
 	}
 }
--- entities/option.go
+++ entities/option.go
@@ -1,6 +1,6 @@
 package entities
 
-// Option represents an option for selection for a single product-variation:
+// Option represents an option for selection for a single Moltin product-variation: https://docs.moltin.com/api/catalog/product-variations/options
 type Option struct {
 	ID          string     `json:"id,omitempty"`
 	Type        string     `json:"type"`
