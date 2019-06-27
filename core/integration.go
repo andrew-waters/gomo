c8069745a26b444ebd9275bf5f7562b53ad4e018
James Carter <james.carter@moltin.com>
2019-06-27 16:39:13 +0100

    Fix JSON marshalling of Integration.Configuration

--- core/integration.go
+++ core/integration.go
@@ -2,17 +2,23 @@ package core
 
 // Integration is a Moltin Integration - https://docs.moltin.com/advanced/events
 type Integration struct {
-	ID              string      `json:"id,omitempty"`
-	Type            string      `json:"type"`
-	Name            string      `json:"name"`
-	Description     string      `json:"description"`
-	Enabled         bool        `json:"enabled"`
-	IntegrationType string      `json:"integration_type"`
-	Observes        []string    `json:"observes"`
-	Configuration   interface{} `json:"configuration"`
+	ID              string                   `json:"id,omitempty"`
+	Type            string                   `json:"type"`
+	Name            string                   `json:"name"`
+	Description     string                   `json:"description"`
+	Enabled         bool                     `json:"enabled"`
+	IntegrationType string                   `json:"integration_type"`
+	Observes        []string                 `json:"observes"`
+	Configuration   IntegrationConfiguration `json:"configuration"`
 	Meta            *struct {
 		Timestamps Timestamps `json:"timestamps,omitempty"`
 	} `json:"meta,omitempty"`
+}
+
+// IntegrationConfiguration specifies a webhook configuration object
+type IntegrationConfiguration struct {
+	URL       string `json:"url"`
+	SecretKey string `json:"secret_key,omitempty"`
 }
 
 // SetType sets the resource type on the struct
