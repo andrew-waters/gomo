4ac4e8a3500efbda147200e10e152e4583c54932
James Carter <james.carter@moltin.com>
2019-06-27 14:22:26 +0100

    Fix JSON marshalling of Customer passwords

--- core/customer.go
+++ core/customer.go
@@ -1,12 +1,40 @@
 package core
 
+import "encoding/json"
+
 // Customer represents a Moltin Customer Entity: https://docs.moltin.com/api/orders-and-customers/customers
 type Customer struct {
-	ID       string      `json:"id,omitempty"`
-	Type     string      `json:"type"`
-	Name     string      `json:"name"`
-	Email    string      `json:"email"`
-	Password interface{} `json:"password"`
+	ID       string            `json:"id,omitempty"`
+	Type     string            `json:"type"`
+	Name     string            `json:"name"`
+	Email    string            `json:"email"`
+	Password *CustomerPassword `json:"password,omitempty"`
+}
+
+// CustomerPassword is a customer's password when making an API request, in
+// which case the Password field should be set, or an indicator that the
+// password was set when reading an API response, in which case Password
+// will be empty and IsSet is indicative.
+type CustomerPassword struct {
+	IsSet    bool
+	Password string
+}
+
+// MarshalJSON marshals a CustomerPassword into JSON
+func (p CustomerPassword) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.Password)
+}
+
+// UnmarshalJSON unmarshals a CustomerPassword from JSON
+func (p *CustomerPassword) UnmarshalJSON(b []byte) error {
+	var isSet bool
+	err := json.Unmarshal(b, &isSet)
+	if err != nil {
+		return err
+	}
+	p.IsSet = isSet
+	p.Password = ""
+	return nil
 }
 
 // SetType sets the resource type on the struct
--- core/customer_test.go
+++ core/customer_test.go
@@ -1,11 +1,118 @@
 package core
 
-import "testing"
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
 
 func TestCustomerType(t *testing.T) {
 	c := Customer{}
 	c.SetType()
 	if c.Type != "customer" {
 		t.Errorf("Customer did not return correct type: `%s`", c.Type)
+	}
+}
+
+func TestCustomerMarshalJSON(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		customer Customer
+		expected string
+	}{
+		{
+			"no password",
+			Customer{
+				Type:  "customer",
+				Name:  "Ron Swanson",
+				Email: "ron@swanson.com",
+			},
+			`{"type":"customer","name":"Ron Swanson","email":"ron@swanson.com"}`,
+		},
+		{
+			"with password",
+			Customer{
+				Type:  "customer",
+				Name:  "Ron Swanson",
+				Email: "ron@swanson.com",
+				Password: &CustomerPassword{
+					Password: "password123",
+				},
+			},
+			`{"type":"customer","name":"Ron Swanson","email":"ron@swanson.com","password":"password123"}`,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			data, err := json.Marshal(test.customer)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(data) != test.expected {
+				t.Errorf(
+					"\nexpected: %s\ngot:       %s\n",
+					test.expected,
+					string(data),
+				)
+			}
+		})
+	}
+}
+
+func TestCustomerUnmarshalJSON(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		json     string
+		expected Customer
+	}{
+		{
+			"no password",
+			`{"type":"customer","name":"Ron Swanson","email":"ron@swanson.com","password":false}`,
+			Customer{
+				Type:  "customer",
+				Name:  "Ron Swanson",
+				Email: "ron@swanson.com",
+				Password: &CustomerPassword{
+					IsSet:    false,
+					Password: "",
+				},
+			},
+		},
+		{
+			"with password",
+			`{"type":"customer","name":"Ron Swanson","email":"ron@swanson.com","password":true}`,
+			Customer{
+				Type:  "customer",
+				Name:  "Ron Swanson",
+				Email: "ron@swanson.com",
+				Password: &CustomerPassword{
+					IsSet:    true,
+					Password: "",
+				},
+			},
+		},
+		{
+			"missing password",
+			`{"type":"customer","name":"Ron Swanson","email":"ron@swanson.com"}`,
+			Customer{
+				Type:  "customer",
+				Name:  "Ron Swanson",
+				Email: "ron@swanson.com",
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var customer Customer
+			err := json.Unmarshal([]byte(test.json), &customer)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(customer, test.expected) {
+				t.Errorf(
+					"\nexpected: %#v\ngot:       %#v\n",
+					test.expected,
+					customer,
+				)
+			}
+		})
 	}
 }
