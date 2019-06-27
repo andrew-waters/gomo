b91cfaf7cad2aeb7f2c7625282c9108112eabe1f
James Carter <james.carter@moltin.com>
2019-06-27 16:04:36 +0100

    Fix JSON marshalling of Modifier

--- core/modifier.go
+++ core/modifier.go
@@ -1,14 +1,166 @@
 package core
 
-// Modifier represents a modifier for a variation option: https://docs.moltin.com/api/catalog/product-variations/modifiers
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Modifier represents a modifier for a variation option:
 type Modifier struct {
-	ID           string      `json:"id,omitempty"`
-	Type         string      `json:"type"`
-	ModifierType string      `json:"modifier_type"`
-	Value        interface{} `json:"value"`
+	ID           string        `json:"id,omitempty"`
+	Type         string        `json:"type"`
+	ModifierType string        `json:"modifier_type"`
+	Value        ModifierValue `json:"value"`
+}
+
+// ModifierValue repesents a number of possible structures depending
+// on the modifier type
+type ModifierValue struct {
+	ModifierValuePlain
+	ModifierValueBuilder
+	ModifierValuePrice
+}
+
+// ModifierValuePlain is used for modifier types: description_equals,
+// description_prepend, description_append, commoditytype, name_equals,
+// name_prepend, name_append, slug_equals, slug_prepend, slug_append,
+// sku_equals, sku_prepend, sku_append, status
+type ModifierValuePlain struct {
+	Value string
+}
+
+// ModifierValueBuilder is used for modifier types: sku_builder, slug_builder
+type ModifierValueBuilder struct {
+	Seek string `json:"seek"`
+	Set  string `json:"set"`
+}
+
+// ModifierValuePrice is used for modifier types: price_increment,
+// price_decrement, price_equals
+type ModifierValuePrice struct {
+	Amount      int    `json:"amount"`
+	Currency    string `json:"currency"`
+	IncludesTax bool   `json:"includes_tax"`
 }
 
 // SetType sets the resource type on the struct
 func (m *Modifier) SetType() {
 	m.Type = modifierType
+}
+
+type modifierValueType int
+
+const (
+	modifierValueTypeInvalid = iota
+	modifierValueTypePlain
+	modifierValueTypeBuilder
+	modifierValueTypePrice
+)
+
+func parseModifierType(mtype string) (t modifierValueType) {
+	switch mtype {
+	case
+		"description_equals",
+		"description_prepend",
+		"description_append",
+		"commoditytype",
+		"name_equals",
+		"name_prepend",
+		"name_append",
+		"slug_equals",
+		"slug_prepend",
+		"slug_append",
+		"sku_equals",
+		"sku_prepend",
+		"sku_append",
+		"status":
+		t = modifierValueTypePlain
+	case
+		"sku_builder",
+		"slug_builder":
+		t = modifierValueTypeBuilder
+	case
+		"price_increment",
+		"price_decrement",
+		"price_equals":
+		t = modifierValueTypePrice
+	default:
+		t = modifierValueTypeInvalid
+	}
+	return
+}
+
+// MarshalJSON marshals a Modifier into JSON
+func (m Modifier) MarshalJSON() ([]byte, error) {
+	mtype := m.ModifierType
+	mod := struct {
+		ID           string      `json:"id,omitempty"`
+		Type         string      `json:"type"`
+		ModifierType string      `json:"modifier_type"`
+		Value        interface{} `json:"value"`
+	}{
+		ID:           m.ID,
+		Type:         m.Type,
+		ModifierType: mtype,
+	}
+	switch parseModifierType(mtype) {
+	case modifierValueTypePlain:
+		mod.Value = m.Value.ModifierValuePlain.Value
+	case modifierValueTypeBuilder:
+		mod.Value = m.Value.ModifierValueBuilder
+	case modifierValueTypePrice:
+		mod.Value = m.Value.ModifierValuePrice
+	default:
+		return nil, fmt.Errorf("unknown modifier type: %s", mtype)
+	}
+	return json.Marshal(mod)
+}
+
+// UnmarshalJSON unmarshals a Modifier from JSON
+func (m *Modifier) UnmarshalJSON(b []byte) error {
+	mod := struct {
+		ID           string `json:"id,omitempty"`
+		Type         string `json:"type"`
+		ModifierType string `json:"modifier_type"`
+	}{}
+	err := json.Unmarshal(b, &mod)
+	if err != nil {
+		return err
+	}
+	mtype := mod.ModifierType
+	m.ID = mod.ID
+	m.Type = mod.Type
+	m.ModifierType = mtype
+	switch parseModifierType(mtype) {
+	case modifierValueTypePlain:
+		value := struct {
+			Value string `json:"value"`
+		}{}
+		err := json.Unmarshal(b, &value)
+		if err != nil {
+			return err
+		}
+		m.Value.ModifierValuePlain.Value = value.Value
+	case modifierValueTypeBuilder:
+		value := struct {
+			Value ModifierValueBuilder `json:"value"`
+		}{}
+		err := json.Unmarshal(b, &value)
+		if err != nil {
+			return err
+		}
+		m.Value.ModifierValueBuilder = value.Value
+	case modifierValueTypePrice:
+		value := struct {
+			Value ModifierValuePrice `json:"value"`
+		}{}
+		err := json.Unmarshal(b, &value)
+		if err != nil {
+			return err
+		}
+		m.Value.ModifierValuePrice = value.Value
+	default:
+		return fmt.Errorf("unknown modifier type: %s", mtype)
+	}
+	return nil
 }
--- core/modifier_test.go
+++ core/modifier_test.go
@@ -1,6 +1,10 @@
 package core
 
-import "testing"
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
 
 func TestModifierType(t *testing.T) {
 	m := Modifier{}
@@ -8,5 +12,132 @@ func TestModifierType(t *testing.T) {
 
 	if m.Type != "modifier" {
 		t.Errorf("Modifier did not return correct type: `%s`", m.Type)
+	}
+}
+
+func TestModifierMarshalJSON(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		modifier Modifier
+		expected string
+	}{
+		{
+			"name_equals",
+			Modifier{
+				Type:         "modifier",
+				ModifierType: "name_equals",
+				Value: ModifierValue{
+					ModifierValuePlain: ModifierValuePlain{"foo"},
+				},
+			},
+			`{"type":"modifier","modifier_type":"name_equals","value":"foo"}`,
+		},
+		{
+			"sku_builder",
+			Modifier{
+				Type:         "modifier",
+				ModifierType: "sku_builder",
+				Value: ModifierValue{
+					ModifierValueBuilder: ModifierValueBuilder{
+						Seek: "foo",
+						Set:  "bar",
+					},
+				},
+			},
+			`{"type":"modifier","modifier_type":"sku_builder","value":{"seek":"foo","set":"bar"}}`,
+		},
+		{
+			"price_equals",
+			Modifier{
+				Type:         "modifier",
+				ModifierType: "price_equals",
+				Value: ModifierValue{
+					ModifierValuePrice: ModifierValuePrice{
+						Amount:      100,
+						Currency:    "USD",
+						IncludesTax: true,
+					},
+				},
+			},
+			`{"type":"modifier","modifier_type":"price_equals","value":{"amount":100,"currency":"USD","includes_tax":true}}`,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			data, err := json.Marshal(test.modifier)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(data) != test.expected {
+				t.Errorf(
+					"\nexpected: %s\ngot:       %s\n",
+					test.expected,
+					string(data),
+				)
+			}
+		})
+	}
+}
+
+func TestModiferUnmarshalJSON(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		json     string
+		expected Modifier
+	}{
+		{
+			"name_equals",
+			`{"type":"modifier","modifier_type":"name_equals","value":"foo"}`,
+			Modifier{
+				Type:         "modifier",
+				ModifierType: "name_equals",
+				Value: ModifierValue{
+					ModifierValuePlain: ModifierValuePlain{"foo"},
+				},
+			},
+		},
+		{
+			"sku_builder",
+			`{"type":"modifier","modifier_type":"sku_builder","value":{"seek":"foo","set":"bar"}}`,
+			Modifier{
+				Type:         "modifier",
+				ModifierType: "sku_builder",
+				Value: ModifierValue{
+					ModifierValueBuilder: ModifierValueBuilder{
+						Seek: "foo",
+						Set:  "bar",
+					},
+				},
+			},
+		},
+		{
+			"price_equals",
+			`{"type":"modifier","modifier_type":"price_equals","value":{"amount":100,"currency":"USD","includes_tax":true}}`,
+			Modifier{
+				Type:         "modifier",
+				ModifierType: "price_equals",
+				Value: ModifierValue{
+					ModifierValuePrice: ModifierValuePrice{
+						Amount:      100,
+						Currency:    "USD",
+						IncludesTax: true,
+					},
+				},
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var modifier Modifier
+			err := json.Unmarshal([]byte(test.json), &modifier)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(modifier, test.expected) {
+				t.Errorf(
+					"\nexpected: %#v\ngot:       %#v\n",
+					test.expected,
+					modifier,
+				)
+			}
+		})
 	}
 }
